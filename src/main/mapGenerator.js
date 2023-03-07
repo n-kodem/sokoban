244c57f48d82eeb0d3e9bc20a5cc8f1f5d08d025
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2023-03-07 20:49:59 +0100

    added loading yaml files

---
+++ src/main/mapGenerator.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import yaml from 'js-yaml'
+class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.level,
+            data: null,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.loadData(this.props.level);
+    }
+
+    // componentDidUpdate(prevProps) {
+    //     if (prevprops.level !== this.props.level) {
+    //         this.loadData(this.props.level);
+    //     }
+    // }
+
+    loadData(id) {
+        fetch(`./maps/${id}.yaml`)
+            .then((response) => response.text())
+            .then((yamlData) => yaml.load(yamlData))
+            .then((parsedData) => this.setState({ data: parsedData, error: null }))
+            .catch((error) => this.setState({ data: null, error: error }));
+    }
+
+    render() {
+        const { data, error } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        }
+
+        if (!data) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <h1>{data.name}</h1>
+                <p>{data.map.join("\n")}</p>
+            </div>
+        );
+    }
+}
+
+export default Map;
